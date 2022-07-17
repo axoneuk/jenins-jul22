d2980568493f1050842de33fa218678c9d01eb3d
axoneuk <info@axone-tech.uk>
2022-07-17 08:58:30 +0100

    Jira 345 - Application change

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,4 +3,8 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("This is my application");
     }
+
+    private void doSomething(){
+        System.out.println("Doing something");
+    }
 }
