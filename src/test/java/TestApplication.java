c4def90d3a2edb4e0cc19dc7287b17db0c559e21
axoneuk <info@axone-tech.uk>
2022-07-17 08:34:40 +0100

    Committing the project first time

---
+++ src/test/java/TestApplication.java
@@ -0,0 +1,25 @@
+import org.junit.Test;
+
+public class TestApplication {
+
+    @Test
+    public void test1(){
+        System.out.println("Running test 1");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("Running test 2");
+    }
+
+    @Test
+    public void test3(){
+        System.out.println("Running test 3");
+    }
+
+    @Test
+    public void test4(){
+        System.out.println("Running test 4");
+    }
+
+}
